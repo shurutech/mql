aef3d4a55e1752e9e743db995344a45c684dc58c
Ashok Choudhary <ashok@shuruapp.com>
2024-03-19 11:19:17 +0530

    create view controller for database page

--- client/app/accounts/database/[id]/page.tsx
+++ client/app/accounts/database/[id]/page.tsx
@@ -1,40 +1,12 @@
 "use client";
 
 import DatabaseInfo from "@/app/components/databaseInfo";
-import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import { getDatabase } from "@/app/lib/service";
-import { toast } from "react-toastify";
-
-type Database = {
-  database_id: string;
-  database_name: string;
-  database_tables: {
-    table_id: string;
-    table_name: string;
-    table_columns: {
-      column_id: string;
-      column_name: string;
-      column_type: string;
-    }[];
-  }[];
-};
+import useDatabaseViewController from "@/app/viewControllers/databaseViewController";
 
 const DatabaseView = () => {
-  const { id } = useParams();
-  const [database, setDatabase] = useState<Database>({} as Database);
-
-  useEffect(() => {
-    const fetchDB = async () => {
-      try {
-        const res = await getDatabase(id);
-        setDatabase(res.data.data);
-      } catch (error) {
-        toast.error("Something went wrong");
-      }
-    };
-    fetchDB();
-  }, []);
+  const {
+    database
+  } = useDatabaseViewController();
 
   return (
     <div>
---
+++ client/app/viewControllers/databaseViewController.ts
@@ -0,0 +1,42 @@
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { getDatabase } from "@/app/lib/service";
+import { toast } from "react-toastify";
+
+type Database = {
+    database_id: string;
+    database_name: string;
+    database_tables: {
+        table_id: string;
+        table_name: string;
+        table_columns: {
+            column_id: string;
+            column_name: string;
+            column_type: string;
+        }[];
+    }[];
+};
+
+const useDatabaseViewController = () => {
+    const { id } = useParams();
+    const [database, setDatabase] = useState<Database>({} as Database);
+  
+    useEffect(() => {
+      const fetchDB = async () => {
+        try {
+          const res = await getDatabase(id);
+          setDatabase(res.data.data);
+        } catch (error) {
+          toast.error("Something went wrong");
+        }
+      };
+      fetchDB();
+    }, []);
+
+    return {
+        database
+    }
+}
+
+export default useDatabaseViewController;
+
