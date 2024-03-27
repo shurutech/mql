38305e72921a82e22976cbe448c8098ca512c76d
Ashok Choudhary <ashok@shuruapp.com>
2024-03-27 19:09:08 +0530

    add api prefix in API endpoints

--- server/app/api/v1/routes.py
+++ server/app/api/v1/routes.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 from app.api.v1 import authentication, databases, query
 
 api_router = APIRouter()
-api_router.include_router(authentication.router, prefix="/v1", tags=["authentication"])
-api_router.include_router(databases.router, prefix="/v1", tags=["databases"])
-api_router.include_router(query.router, prefix="/v1", tags=["query"])
+api_router.include_router(authentication.router, prefix="/api/v1", tags=["authentication"])
+api_router.include_router(databases.router, prefix="/api/v1", tags=["databases"])
+api_router.include_router(query.router, prefix="/api/v1", tags=["query"])
