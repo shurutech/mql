0dc8a710a7404be965ebe898fdf98d8844296f57
Vijay Goswami <vijay@shurutech.com>
2024-04-02 18:32:38 +0530

    rename folder to test_data

--- server/app/tests/test_api/v1/test_query.py
+++ server/app/tests/test_api/v1/test_query.py
@@ -7,6 +7,10 @@ from app.crud.crud_user_database import crud_user_database
 from app.crud.crud_query_history import crud_query_history
 from app.schemas.user_database import UserDatabase as UserDatabaseSchema
 from app.schemas.query_history import QueryHistory as QueryHistorySchema
+from app.clients.openai_client import OpenAIClient 
+from app.crud.crud_user_database import CRUDUserDatabase
+
+
 
 mock_openai_embedding_response = {
     "data": [
@@ -16,22 +20,27 @@ mock_openai_embedding_response = {
 }
 
 mock_openai_chat_response = {
-    "id": "chatcmpl-123",
-    "object": "chat.completion",
-    "created": 1677652288,
-    "model": "gpt-4",
-    "choices": [
+    'id': 'chatcmpl-99TiMJDtVEyznHcIbhatdpz9mYbzV',
+    'choices': [
         {
-            "index": 0,
-            "message": {
-                "role": "system",
-                "content": "SELECT * FROM employee",
-            },
-            "finish_reason": "stop",
+            'finish_reason': 'stop',
+            'index': 0,
+            'message': {
+                'content': "SELECT * FROM employee",
+                'role': 'system',
+            }
         }
     ],
-    "usage": {"prompt_tokens": 9, "completion_tokens": 12, "total_tokens": 21},
+    'created': 1712046202,
+    'model': 'gpt-4-0613',
+    'object': 'chat.completion',
+    'usage': {
+        'completion_tokens': 64, 
+        'prompt_tokens': 356, 
+        'total_tokens': 420
+    }
 }
+
 
 mock_openai_chat_response_with_connection_db = {
     "id": "chatcmpl-123",
@@ -73,57 +82,6 @@ def mock_chat_response_with_connection_db(*args, **kwargs) -> dict:
 def mock_database_connection(*args, **kwargs) -> dict:
     return mock_database_connection_response   
 
-
-def test_query(
-    client: TestClient, db: Session, valid_jwt: str, valid_user_model: UserModel
-) -> None:
-    headers = {"Authorization": f"Bearer {valid_jwt}"}
-
-    with patch(
-        "app.services.embeddings_service.embeddings_service.create_embeddings",
-        side_effect=None,
-    ):
-        with open("output_schema.txt", "rb") as file:
-            response = client.post(
-                "/v1/databases",
-                files={"file": file},
-                data={"database_name": "Test"},
-                headers=headers,
-            )
-
-    assert response.status_code == 202
-    database_id = crud_user_database.get_by_user_id(db=db, user_id=valid_user_model.id)[
-        0
-    ].id
-    with (
-        patch(
-            "app.clients.openai_client.chat.completions.create",
-            side_effect=mock_chat_response,
-        ),
-        patch(
-            "app.clients.openai_client.openai.embeddings.create",
-            side_effect=mock_embedding_create,
-        ),
-    ):
-        response = client.post(
-            f"/v1/query/{database_id}",
-            headers={
-                "Content-Type": "application/x-www-form-urlencoded",
-                "Authorization": f"Bearer {valid_jwt}",
-            },
-            data={"nl_query": "show me the table schema of the table employee"},
-        )
-
-    assert response.status_code == 200
-    assert response.json() == {
-        "message": "Query processed successfully",
-        "data": {
-            "sql_query": "SELECT * FROM employee",
-            "nl_query": "show me the table schema of the table employee",
-        },
-    }
-
-
 def test_query_history(
     client: TestClient, db: Session, valid_jwt: str, valid_user_model: UserModel
 ) -> None:
@@ -142,8 +100,9 @@ def test_query_history(
             sql_query="SELECT * FROM employee",
         ),
     )
+
     response = client.get(
-        f"/v1/query/{database_obj.id}/history",
+        f"/api/v1/query-history?db_id={database_obj.id}",
         headers={"Authorization": f"Bearer {valid_jwt}"},
     )
     assert response.status_code == 200
@@ -185,7 +144,7 @@ def test_query_history_by_id(
         ),
     )
     response = client.get(
-        f"/v1/query/{database_obj.id}/history/{query_history_obj.id}",
+        f"/api/v1/query-history/{query_history_obj.id}", 
         headers={"Authorization": f"Bearer {valid_jwt}"},
     )
     assert response.status_code == 200
@@ -200,62 +159,6 @@ def test_query_history_by_id(
                 "created_at": query_history_obj.created_at.isoformat(),
                 "updated_at": query_history_obj.updated_at.isoformat(),
         }
-            
-        }
-    }
-
-def test_query_for_connected_database(
-    client: TestClient, db: Session, valid_jwt: str, valid_user_model: UserModel
-) -> None:
-    headers = {"Authorization": f"Bearer {valid_jwt}"}
-    test_data = {
-        "database_name": "analytics_test",
-        "database_user": "shuru",
-        "database_password": "password",
-        "database_host": "localhost",
-        "database_port": "5432",
-    }
-
-    with patch(
-        "app.services.embeddings_service.embeddings_service.create_embeddings",
-        side_effect=None,
-    ):
-            response = client.post(
-            "/v1/databases/connect",
-            data=test_data,
-            headers=headers,
-        )
-    assert response.status_code == 202
-    database_id = crud_user_database.get_by_user_id(db=db, user_id=valid_user_model.id)[
-        0
-    ].id
-    with (
-        patch(
-            "app.clients.openai_client.chat.completions.create",
-            side_effect=mock_chat_response_with_connection_db,
-        ),
-        patch(
-            "app.clients.openai_client.openai.embedding.create",
-            side_effect=mock_embedding_create,
-        ),
-    ):
-        response = client.post(
-            f"/v1/query/{database_id}",
-            headers={
-                "Content-Type": "application/x-www-form-urlencoded",
-                "Authorization": f"Bearer {valid_jwt}",
-            },
-            data={"nl_query": "show me the number of users in the table users","execute":True},
-        )
-
-    assert response.status_code == 200
-    assert response.json() == {
-        "message": "Query processed successfully",
-        "data": {
-            
-            "sql_query": "SELECT count(*) FROM users",
-            "nl_query": "show me the number of users in the table users",
-            'query_result': [{'count': 1}],
             
         }
     }
